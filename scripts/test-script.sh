771ea29c8261cbdbf21628249296afc4d99d8b03
Glotov Ilya <erlendum.ds@gmail.com>
2024-11-23 15:53:21 +0300

    Update test-script.sh

--- scripts/test-script.sh
+++ scripts/test-script.sh
@@ -11,8 +11,9 @@ ssh_user=${5:-${SSH_USER}}
 path=$(dirname "$0")
 
 temp_key=$(mktemp)
-echo "$ssh_private_key"
 echo "$ssh_private_key" > "$temp_key"
+echo "$temp_key"
+cat "$temp_key"
 chmod 600 "$temp_key"
 
 timed() {
